2cd3f11c8317e288c0977b135e99eaf649e3a3fe
pwilczynskiclearcode <p.wilczynski@clearcode.cc>
2013-11-12 11:30:01 +0100

    Adds missing licence

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+# Copyright (C) 2013 by Clearcode <http://clearcode.cc>
+# and associates (see AUTHORS).
+#
+# This file is part of django-data-faker.
+#
+# django-data-faker is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# django-data-faker is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with django-data-faker.  If not, see <http://www.gnu.org/licenses/>.
+
 from setuptools import setup
 
 setup(
